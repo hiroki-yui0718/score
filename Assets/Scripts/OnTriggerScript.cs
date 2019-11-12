8d27192a5d639984670a198c12827ebcccbcbed8
hiroki-yui0718 <xxx_yyy_www_1234@yahoo.co.jp>
2019-11-12 12:00:49 +0900

    RealTime Score Change Success!

--- Assets/Scripts/OnTriggerScript.cs
+++ Assets/Scripts/OnTriggerScript.cs
@@ -6,8 +6,12 @@ using UnityEngine.UI;
 public class OnTriggerScript : MonoBehaviour
 {
     private int time;
-    private int score;
+    public Text scoreText;
+    public static int score;
     public GameObject cube1;
+    float seconds1;
+    float seconds2;
+    float seconds3;
     System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
 
     // Start is called before the first frame update
@@ -19,39 +23,42 @@ public class OnTriggerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
     }
     void OnCollisionEnter(Collision collision)
     {
-
         sw.Start();
     }
         void OnCollisionStay(Collision collision)
     {
-        time = sw.Elapsed.Seconds;
-        if (time > 10)
-        {
+        seconds1 += Time.deltaTime;
+        if (seconds1 >= 10.0f)
+        { 
+            scoreText.text = (int.Parse(scoreText.text) + 100).ToString();
             cube1.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("10秒経過");
+            seconds1 = 0.0f;
+        }
+        seconds2 += Time.deltaTime;
+        if (seconds2 >= 10.0f)
+        {
+            seconds3 += Time.deltaTime;
+            if (seconds3 >= 3.0f)
+            {
+                scoreText.text = (int.Parse(scoreText.text) + 10).ToString();
+                seconds3 = 0.0f;
+                seconds1 = 0.0f;
+                Debug.Log("3秒経過");
+            }
         }
 
         GameObject.Find("Plus").GetComponent<Text>().text = sw.Elapsed.Seconds.ToString();
     }
     void OnCollisionExit(Collision other)
     {
-//        if (time == 10)
-//        {
-//            score = 100;
-//            int plusScore = int.Parse(GameObject.Find("SCORE").GetComponent<Text>().text) + score;
-//            GameObject.Find("SCORE").GetComponent<Text>().text = plusScore.ToString();
-//        }
-
-        if (time > 10)
-        {
-     
-            score = 100 + (int)((sw.Elapsed.Seconds-10) / 3) * 10;
-            int plusScore = int.Parse(GameObject.Find("SCORE").GetComponent<Text>().text) + score;
-            GameObject.Find("SCORE").GetComponent<Text>().text = plusScore.ToString();
-         
-        }
+        seconds1 = 0.0f;
+        seconds2 = 0.0f;
+        seconds3 = 0.0f;
         sw.Stop();
         sw.Reset();
         cube1.gameObject.GetComponent<Renderer>().material.color = Color.black;
